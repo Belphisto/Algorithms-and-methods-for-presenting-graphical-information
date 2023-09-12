1d13d79aa25ca05b7d79829577c0d2131cd71c81
Belphisto <tiniorion@mail.ru>
2023-09-12 14:02:45 +0500

    create lab1

---
+++ WindowsFormsLab1_BlinovaEM_KE-404/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace WindowsFormsLab1_BlinovaEM_KE_404
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip3 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.task1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.task1ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.task11ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.task12ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.task2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.task22ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.task23ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip3.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+            // 
+            // contextMenuStrip3
+            // 
+            this.contextMenuStrip3.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip3.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.task1ToolStripMenuItem});
+            this.contextMenuStrip3.Name = "contextMenuStrip3";
+            this.contextMenuStrip3.Size = new System.Drawing.Size(113, 28);
+            // 
+            // task1ToolStripMenuItem
+            // 
+            this.task1ToolStripMenuItem.Name = "task1ToolStripMenuItem";
+            this.task1ToolStripMenuItem.Size = new System.Drawing.Size(112, 24);
+            this.task1ToolStripMenuItem.Text = "task1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.task1ToolStripMenuItem1,
+            this.task2ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(482, 28);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // task1ToolStripMenuItem1
+            // 
+            this.task1ToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.task11ToolStripMenuItem,
+            this.task12ToolStripMenuItem});
+            this.task1ToolStripMenuItem1.Name = "task1ToolStripMenuItem1";
+            this.task1ToolStripMenuItem1.Size = new System.Drawing.Size(59, 24);
+            this.task1ToolStripMenuItem1.Text = "Файл";
+            // 
+            // task11ToolStripMenuItem
+            // 
+            this.task11ToolStripMenuItem.Name = "task11ToolStripMenuItem";
+            this.task11ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.task11ToolStripMenuItem.Text = "Редактирование";
+            // 
+            // task12ToolStripMenuItem
+            // 
+            this.task12ToolStripMenuItem.Name = "task12ToolStripMenuItem";
+            this.task12ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.task12ToolStripMenuItem.Text = "Выход";
+            this.task12ToolStripMenuItem.Click += new System.EventHandler(this.task12ToolStripMenuItem_Click);
+            // 
+            // task2ToolStripMenuItem
+            // 
+            this.task2ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.task22ToolStripMenuItem,
+            this.task23ToolStripMenuItem});
+            this.task2ToolStripMenuItem.Name = "task2ToolStripMenuItem";
+            this.task2ToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.task2ToolStripMenuItem.Text = "Справка";
+            // 
+            // task22ToolStripMenuItem
+            // 
+            this.task22ToolStripMenuItem.Name = "task22ToolStripMenuItem";
+            this.task22ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.task22ToolStripMenuItem.Text = "Автор программы";
+            // 
+            // task23ToolStripMenuItem
+            // 
+            this.task23ToolStripMenuItem.Name = "task23ToolStripMenuItem";
+            this.task23ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.task23ToolStripMenuItem.Text = "О программе";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 336);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip3.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip3;
+        private System.Windows.Forms.ToolStripMenuItem task1ToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem task1ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem task11ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem task12ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem task2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem task22ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem task23ToolStripMenuItem;
+    }
+}
+
